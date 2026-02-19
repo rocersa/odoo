15eec8c354e40f18ec834018bdda1e03b65a2ddd
Harry <harry@harrybird.nz>
2026-02-19 15:22:08 +1300

    removed website quote old

--- eg_website_quote_old/models/res_config_settings.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, fields
-
-
-class ResConfigSettings(models.TransientModel):
-    _inherit = 'res.config.settings'
-
-    is_disable_checkout = fields.Boolean(string="Disable Proceed to Checkout", related='website_id.is_disable_checkout',
-                                         readonly=False)
--- eg_website_quote_old/models/sale_order.py
+++
@@ -1,7 +0,0 @@
-from odoo import models, fields
-
-
-class SaleOrder(models.Model):
-    _inherit = 'sale.order'
-
-    is_rfq_from_website = fields.Boolean(string="Request from Website")
