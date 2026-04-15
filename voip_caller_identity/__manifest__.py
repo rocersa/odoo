b71c18b9e8445053502742cf35e756e014012cce
Harry <harry@harrybird.nz>
2026-04-15 10:22:35 +1000

    [FIX] voip_caller_identity: fix call log view errors
    - Replace datetime.timedelta with relativedelta in search filter
    - Fix voip.menu_voip_config reference in call log menu
    Version bump to 19.0.1.0.6.

--- voip_caller_identity/__manifest__.py
+++ voip_caller_identity/__manifest__.py
@@ -17,7 +17,7 @@
     """,
     "author": "Harry",
     "category": "Productivity/Phone",
-    "version": "19.0.1.0.5",
+    "version": "19.0.1.0.6",
     "depends": ["voip", "crm"],
     "data": [
         "security/voip_caller_identity_security.xml",
