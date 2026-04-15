4caac5cd250f12952119ebf3c465e7041393af75
Harry <harry@harrybird.nz>
2026-04-15 10:49:15 +1000

    voip_caller_identity: fix search view group tag syntax

--- voip_caller_identity/__manifest__.py
+++ voip_caller_identity/__manifest__.py
@@ -17,7 +17,7 @@
     """,
     "author": "Harry",
     "category": "Productivity/Phone",
-    "version": "19.0.1.0.9",
+    "version": "19.0.1.0.10",
     "depends": ["voip", "crm"],
     "data": [
         "security/voip_caller_identity_security.xml",
