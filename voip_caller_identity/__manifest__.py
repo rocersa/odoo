cdb6e0a15355d7fc5c9608edf6fa79023db6bbb3
Harry <harry@harrybird.nz>
2026-04-15 11:06:45 +1000

    voip_caller_identity: make caller identity list editable for inline creation

--- voip_caller_identity/__manifest__.py
+++ voip_caller_identity/__manifest__.py
@@ -17,7 +17,7 @@
     """,
     "author": "Harry",
     "category": "Productivity/Phone",
-    "version": "19.0.1.0.13",
+    "version": "19.0.1.0.14",
     "depends": ["voip", "crm"],
     "data": [
         "security/voip_caller_identity_security.xml",
