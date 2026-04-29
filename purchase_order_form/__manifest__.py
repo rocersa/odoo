470cc3542ef188b75fb3076e17fd5ac0e8c601d4
Harry <harry@harrybird.nz>
2026-04-29 14:39:47 +1000

    purchase_order_form: add weight fields and sortable order lines by product name

---
+++ purchase_order_form/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Purchase Order Form",
+
+    'summary': """
+        Enhances the purchase order form with weight totals and sortable lines.
+    """,
+
+    'description': """
+        Enhances the purchase order form with:
+        - A computed weight field on each order line
+        - A total weight summary on the purchase order
+        - Order lines sorted by product name by default
+    """,
+
+    'author': "Harry",
+
+    'category': 'Purchase Management',
+    'version': '0.1',
+
+    'depends': ['purchase', 'stock'],
+    'application': True,
+    'installable': True,
+    'data': [
+        'views/purchase_order_view.xml',
+    ],
+    'license': 'AGPL-3'
+}
