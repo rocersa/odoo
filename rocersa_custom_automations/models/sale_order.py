6828e128968f7597522ded651ddc1cc42428a9af
Harry <harry@harrybird.nz>
2026-05-06 15:00:09 +1000

    rocersa_custom_automations: block quote send without shipping method

--- rocersa_custom_automations/models/sale_order.py
+++ rocersa_custom_automations/models/sale_order.py
@@ -1,13 +1,26 @@
 from odoo import _, models
+from odoo.exceptions import UserError
 
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
+    def _check_shipping_method(self):
+        """Return an error message if the order lacks a shipping method."""
+        self.ensure_one()
+        if not self.carrier_id and not self.is_all_service:
+            return _("Please select a shipping method before confirming this order.")
+        return False
+
     def _confirmation_error_message(self):
         error = super()._confirmation_error_message()
         if error:
             return error
-        if not self.carrier_id and not self.is_all_service:
-            return _("Please select a shipping method before confirming this order.")
-        return False
+        return self._check_shipping_method()
+
+    def action_quotation_send(self):
+        for order in self:
+            error = order._check_shipping_method()
+            if error:
+                raise UserError(error)
+        return super().action_quotation_send()
