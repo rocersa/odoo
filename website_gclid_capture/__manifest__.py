917872d3071cd2aa637118f4d995afc3749251a2
Harry <harry@harrybird.nz>
2026-08-14 09:54:13 +1000

    Add website_gclid_capture module: first-party gclid cookie for ads attribution

---
+++ website_gclid_capture/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Website GCLID Capture",
+    "summary": "Capture the Google Ads gclid as a first-party cookie for attribution",
+    "version": "19.0.1.0.1",
+    "category": "Website",
+    "author": "Rocersa",
+    "license": "LGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "website",
+    ],
+    "data": [
+        "views/website_templates.xml",
+    ],
+}
